bceec3d51cf622b0528565cf6ad9b9adfb0cb82a
developertenzin <tenzinphuljung7@gmail.com>
2015-11-29 18:28:16 -0600

    Solved Problem 12, however, the solution is not yet efficient. Takes a while

--- highly_divisble_triangular_number/highly_divisible_triangular_number.rb
+++ highly_divisble_triangular_number/highly_divisible_triangular_number.rb
@@ -1,4 +1,19 @@
 def highly_divisible_triangular_number
+  temp = 0
+  x = 0
+  loop do
+    x += 1
+    temp += x
+    triangle_num_list = []
+    (1..temp).each do |num|
+      triangle_num_list.push(num) if temp % num == 0
+    end
+  puts triangle_num_list[-1] if triangle_num_list.length > 500
+  break if triangle_num_list.length > 500
+  end
 
 
 end
+
+highly_divisible_triangular_number
+
